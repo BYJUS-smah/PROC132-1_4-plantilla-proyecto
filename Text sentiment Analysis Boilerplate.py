a674079ea46e35082fc3cc20d3934a45c01bc2ef
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-01-04 12:31:04 +0530

    Add files via upload

---
+++ Text sentiment Analysis Boilerplate.py
@@ -0,0 +1,18 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["Awesome Camera. Worth the price"]
+
+# Tokenization
+
+# Create a word_index dictionary
+
+# Padding the sequence
+
+# Define the model using .h5 file
+
+# Test the model
+
+# Print the result
